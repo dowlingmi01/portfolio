a584863a0ccd7fc2a4d19cdde4d09de4e1c9d37c
dowlingmi01 <michaeldowling440@gmail.com>
2022-09-22 16:27:23 -0600

    implemented create, edit, update and show actions

--- app/controllers/folios_controller.rb
+++ app/controllers/folios_controller.rb
@@ -1,2 +1,42 @@
 class FoliosController < ApplicationController
+  def index
+    @folio_items = Folio.all
+  end
+
+  def new
+    @folio_item = Folio.new
+  end
+
+  def create
+    @folio_item = Folio.new(params.require(:folio).permit(:title, :subtitle, :body))
+
+    respond_to do |format|
+      if @folio_item.save
+        format.html { redirect_to folios_path, notice: "Folio was successfully created." }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def edit
+    @folio_item = Folio.find(params[:id])
+  end
+
+  def update
+    @folio_item = Folio.find(params[:id])
+
+    respond_to do |format|
+      if @folio_item.update(params.required(:folio).permit(:title, :subtitle, :body,))
+        format.html { redirect_to folios_path, notice: "Portfolio was successfully updated." }
+      else
+        format.html { render :edit, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def show
+    @folio_item = Folio.find(params[:id])
+  end
+
 end
