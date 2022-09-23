5099fab5340a030b6b6d6f0eb4f3fd05f584d3ec
dowlingmi01 <michaeldowling440@gmail.com>
2022-09-23 13:08:49 -0600

    implemented toggle status for blogs using enum

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,12 @@ Rails.application.routes.draw do
   get 'contact', to: 'pages#contact'
 
 
-  resources :blogs
+  resources :blogs do
+    member do
+      get :toggle_status
+    end
+  end
+
   get 'tech-news', to: 'pages#tech_news'
 
   root to: 'pages#home'
